541912798036e6717996997a98fb8bd4ad5f8bd4
piedcipher <pateltirth454@gmail.com>
2025-10-22 19:31:49 +0530

    Replaced page_flip w page_curl_effect

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:page_flip/page_flip.dart';
+import 'package:page_curl_effect/page_curl_effect.dart';
 import 'package:tirth_today/pages/art_video_player_page.dart';
 import 'package:tirth_today/pages/home_page.dart';
 import 'package:tirth_today/pages/work_experience_page.dart';
@@ -36,9 +36,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return PageFlipWidget(
-      lastPage: ArtVideoPlayer(),
-      children: [HomePage(), WorkExperiencePage(), ArtVideoPlayer()],
+    return PageCurlEffect(
+      pages: [HomePage(), WorkExperiencePage(), ArtVideoPlayer()],
+      pageCurlController: PageCurlController(
+        Size(
+          MediaQuery.of(context).size.width,
+          MediaQuery.of(context).size.height,
+        ),
+        pageCurlIndex: Pages.home.index,
+        numberOfPage: Pages.values.length,
+      ),
     );
   }
 }
