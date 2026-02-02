4e72024b5fa5b204a626dcda242d5c8e7b14cb0d
piedcipher <pateltirth454@gmail.com>
2026-02-02 20:47:30 +0530

    Revert to original

--- lib/layouts/notebook_layout.dart
+++ lib/layouts/notebook_layout.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:get_it/get_it.dart';
-import 'package:page_curl_effect/page_curl_effect.dart';
-import 'package:tirth_today/pages/art_video_player_page.dart';
 import 'package:tirth_today/utils/constants.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class NotebookLayout extends StatelessWidget {
   const NotebookLayout({super.key});
@@ -66,35 +61,7 @@ class NotebookLayout extends StatelessWidget {
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 30),
-            if (GetIt.I.get<PageCurlController>().pageCurlIndex == 0)
-              OverflowBar(
-                spacing: 16,
-                overflowSpacing: 16,
-                children: [
-                  FilledButton(
-                    onPressed: () async {
-                      launchUrl(
-                        Uri.parse(
-                          'https://tirth.today/assets/assets/Tirth-Patel-Resume.pdf',
-                        ),
-                      );
-                    },
-                    child: Text('View Resume'),
-                  ),
-                  FilledButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const ArtVideoPlayer(),
-                        ),
-                      );
-                    },
-                    child: Text('View Art'),
-                  ),
-                ],
-              ),
-            const SizedBox(height: 30),
+            const SizedBox(height: 60),
             DecoratedBox(
               decoration: BoxDecoration(color: AppColors.notebookRed),
               child: const SizedBox(height: 1.5, width: double.infinity),
@@ -114,23 +81,7 @@ class NotebookLayout extends StatelessWidget {
               decoration: BoxDecoration(color: AppColors.notebookRed),
               child: const SizedBox(height: 1.5, width: double.infinity),
             ),
-            const SizedBox(height: 30),
-            if (GetIt.I.get<PageCurlController>().pageCurlIndex == 0)
-              OverflowBar(
-                spacing: 16,
-                overflowSpacing: 16,
-                children: [
-                  FilledButton(
-                    onPressed: () {
-                      GetIt.I<PageCurlController>().pageCurlIndex =
-                          GetIt.I<PageCurlController>().getNextPageIndex() ?? 0;
-                      GetIt.I<VoidCallback>()();
-                    },
-                    child: Text('Flip Page'),
-                  ),
-                ],
-              ),
-            const SizedBox(height: 30),
+            const SizedBox(height: 60),
           ],
         ),
       ],
