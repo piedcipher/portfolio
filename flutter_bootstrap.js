514986d87b7e68d941f9921e760b8dfd23185410
Tirth Patel <tirthpatel@Tirths-MacBook-Pro.local>
2026-02-19 22:13:31 +0530

    Built web
    Branch: main
    Commit: 938982c93ef892345602ffdba51b305b86f86d1b
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"06df71c51446e96939c6a615b7c34ce9123806
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4071454756" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "221016851" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
