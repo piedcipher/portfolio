2b7300b4cd7410d800a392f8c7c28cc9fcffe420
piedcipher <pateltirth454@gmail.com>
2026-03-05 19:31:30 +0530

    Remove Web

--- lib/pages/work_experience_page.dart
+++ lib/pages/work_experience_page.dart
@@ -18,7 +18,7 @@ class WorkExperiencePage extends StatelessWidget {
         "company": "Nevercode HQ / Codemagic",
         "duration": "Feb 2026 - Current",
         "description":
-            "Supporting developers using Codemagic's CI/CD platform for Flutter and mobile apps by triaging complex build and deployment issues across Android, iOS, and web pipelines. Reproduce failures, analyze logs, debug environment and configuration problems, and collaborate with engineering to drive product improvements that enhance build reliability and developer experience.",
+            "Supporting developers using Codemagic's CI/CD platform for Flutter and mobile apps by triaging complex build and deployment issues across Mobile Dev pipelines. Reproduce failures, analyze logs, debug environment and configuration problems, and collaborate with engineering to drive product improvements that enhance build reliability and developer experience.",
       },
       {
         "role": "Open Source Software Engineer",
