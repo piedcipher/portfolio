8161e0b3a0e866c86e19c86bad2770223d124e30
piedcipher <pateltirth454@gmail.com>
2026-09-04 05:33:47 +0530

    Fix twitter_embed.dart export statement for js_interop support

--- lib/widgets/twitter_embed.dart
+++ lib/widgets/twitter_embed.dart
@@ -1,2 +1,3 @@
 export 'twitter_embed_stub.dart'
-    if (dart.library.html) 'twitter_embed_web.dart';
+    if (dart.library.html) 'twitter_embed_web.dart'
+    if (dart.library.js_interop) 'twitter_embed_web.dart';
